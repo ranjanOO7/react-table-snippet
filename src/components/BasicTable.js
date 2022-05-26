658ebcc0f92692ab15f10223c684c706874ee032
Ashwani Ranjan <ranjan062001@gmail.com>
2022-05-26 17:22:27 +0530

    Initialized the app

---
+++ src/components/BasicTable.js
@@ -0,0 +1,15 @@
+import React, { useMemo } from "react";
+import { useTable } from "react-table";
+import MOCK_DATA from "./MOCK_DATA.json";
+import { COLUMNS } from "./columns";
+
+export const BasicTable = () => {
+    const columns = useMemo(() => COLUMNS, []);
+    const data = useMemo(() => MOCK_DATA, []);
+
+    useTable({
+        columns,
+        data,
+    });
+    return <div></div>;
+};
