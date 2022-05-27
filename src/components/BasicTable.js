77cfed21255b779c5e29141dda8e16877b4e187b
Ashwani Ranjan <ranjan062001@gmail.com>
2022-05-28 00:28:02 +0530

    Basic table using react-table

--- src/components/BasicTable.js
+++ src/components/BasicTable.js
@@ -2,14 +2,55 @@ import React, { useMemo } from "react";
 import { useTable } from "react-table";
 import MOCK_DATA from "./MOCK_DATA.json";
 import { COLUMNS } from "./columns";
+import "./table.css";
 
 export const BasicTable = () => {
     const columns = useMemo(() => COLUMNS, []);
     const data = useMemo(() => MOCK_DATA, []);
 
-    useTable({
-        columns,
-        data,
-    });
-    return <div></div>;
+    const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
+        useTable({
+            columns,
+            data,
+        });
+
+    // const  =
+    //     tableInstance;
+    return (
+        <>
+            <table {...getTableProps()}>
+                <thead>
+                    {headerGroups.map((headerGroup) => (
+                        <tr {...headerGroup.getHeaderGroupProps()}>
+                            {headerGroup.headers.map((column) => (
+                                <th {...column.getHeaderProps()}>
+                                    {column.render("Header")}
+                                </th>
+                            ))}
+                            <td></td>
+                        </tr>
+                    ))}
+                </thead>
+                <tbody {...getTableBodyProps()}>
+                    {rows.map((row) => {
+                        prepareRow(row);
+                        return (
+                            <tr {...row.getRowProps()}>
+                                {row.cells.map((cell) => {
+                                    return (
+                                        <td {...cell.getCellProps()}>
+                                            {cell.render("Cell")}
+                                        </td>
+                                    );
+                                })}
+                            </tr>
+                        );
+                    })}
+                    <tr>
+                        <td></td>
+                    </tr>
+                </tbody>
+            </table>
+        </>
+    );
 };
